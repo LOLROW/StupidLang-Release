b0024698887fe4ccaad233facac5c2a6f0f7328e
LOLROW <32370118+LOLROW@users.noreply.github.com>
2021-09-01 11:42:21 -0500

    Update StupidLang.java

--- StupidLang.java
+++ StupidLang.java
@@ -1,42 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class StupidLang {
+public class Main {
     public static int[] memory = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     public static int memoryState = memory[0];
     public static void main(String[] args) throws IOException {
         Scanner keyboard = new Scanner(System.in);
-        File file = new File(""); // Put filepath of StupidLang file.
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String output;
-        while ((output = br.readLine()) != null) {
-            String[] codeLine = output.split(" ");
-            for (int i = 0; i < codeLine.length; i++) {
-                if (codeLine[i].equals("ADD") || codeLine[i].equals("add")) {
-                    int num = Integer.parseInt(codeLine[i + 1]);
-                    memory[memoryState] += num;
-                }
-                else if (codeLine[i].equals("SUB") || codeLine[i].equals("sub")) {
-                    int num = Integer.parseInt(codeLine[i + 1]);
-                    memory[memoryState] -= num;
-                }
-                else if (codeLine[i].equals("PRINT") || codeLine[i].equals("print")) {
-                    System.out.print(Character.toChars(memory[memoryState]));
-                }
-                else if (codeLine[i].equals("=>")) {
-                    memoryState += 1;
-                }
-                else if (codeLine[i].equals("<=")) {
-                    memoryState -= 1;
-                }
-                else if (codeLine[i].equals("INPUT") || codeLine[i].equals("input")) {
-                    char input = keyboard.nextLine().charAt(0);
-                    String a = Integer.toHexString(input);
-                    int conv = Integer.parseInt(a);
-                    memory[memoryState] += conv;
-                }
-                else if (codeLine[i].equals("LOOP:") || codeLine[i].equals("loop:")) {
-                    loopMethod(codeLine);
+        File file = new File("C:\\Users\\strader_883193\\IdeaProjects\\first\\src\\examplecode.txt"); // Put filepath of StupidLang file.
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String output;
+            while ((output = br.readLine()) != null) {
+                String[] codeLine = output.split(" ");
+                for (int i = 0; i < codeLine.length; i++) {
+                    switch (codeLine[i]) {
+                        case "ADD", "add" -> {
+                            int num = Integer.parseInt(codeLine[i + 1]);
+                            memory[memoryState] += num;
+                            break;
+                        }
+                        case "SUB", "sub" -> {
+                            int num = Integer.parseInt(codeLine[i + 1]);
+                            memory[memoryState] -= num;
+                            break;
+                        }
+                        case "PRINT", "print" -> System.out.print(Character.toChars(memory[memoryState]));
+                        case "=>" -> memoryState += 1;
+                        case "<=" -> memoryState -= 1;
+                        case "INPUT", "input" -> {
+                            char input = keyboard.nextLine().charAt(0);
+                            String a = Integer.toHexString(input);
+                            int conv = Integer.parseInt(a);
+                            memory[memoryState] += conv;
+                        }
+                        case "LOOP:", "loop:" -> loopMethod(codeLine);
+                    }
                 }
             }
         }
@@ -52,31 +53,27 @@ public class StupidLang {
                     finished = true;
                 }
                 else {
-                    if (codeLine[x].equals("ADD") || codeLine[x].equals("add")) {
-                        int num = Integer.parseInt(codeLine[x + 1]);
-                        memory[memoryState] += num;
-                    }
-                    else if (codeLine[x].equals("SUB") || codeLine[x].equals("sub")) {
-                        int num = Integer.parseInt(codeLine[x + 1]);
-                        memory[memoryState] -= num;
-                    }
-                    else if (codeLine[x].equals("PRINT") || codeLine[x].equals("print")) {
-                        System.out.print(Character.toChars(memory[memoryState]));
-                    }
-                    else if (codeLine[x].equals("=>")) {
-                        memoryState += 1;
-                    }
-                    else if (codeLine[x].equals("<=")) {
-                        memoryState -= 1;
-                    }
-                    else if (codeLine[x].equals("INPUT") || codeLine[x].equals("input")) {
-                        char input = keyboard.nextLine().charAt(0);
-                        String a = Integer.toHexString(input);
-                        int conv = Integer.parseInt(a);
-                        memory[memoryState] += conv;
-                    }
-                    else if (codeLine[x].equals("LOOP:") || codeLine[x].equals("loop:")) {
-                        loopMethod(codeLine);
+                    switch (codeLine[x]) {
+                        case "ADD", "add" -> {
+                            int num = Integer.parseInt(codeLine[x + 1]);
+                            memory[memoryState] += num;
+                            break;
+                        }
+                        case "SUB", "sub" -> {
+                            int num = Integer.parseInt(codeLine[x + 1]);
+                            memory[memoryState] -= num;
+                            break;
+                        }
+                        case "PRINT", "print" -> System.out.print(Character.toChars(memory[memoryState]));
+                        case "=>" -> memoryState += 1;
+                        case "<=" -> memoryState -= 1;
+                        case "INPUT", "input" -> {
+                            char input = keyboard.nextLine().charAt(0);
+                            String a = Integer.toHexString(input);
+                            int conv = Integer.parseInt(a);
+                            memory[memoryState] += conv;
+                        }
+                        case "LOOP:", "loop:" -> loopMethod(codeLine);
                     }
                 }
             }
